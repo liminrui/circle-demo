055f8588ef9fd1ec5d28ccc4b2db13fcf0a958c3
Admin <209695793@qq.com>
2024-02-22 14:12:30 +0800

    添加自动化部署

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,10 +14,8 @@ echo 'create success'
 cd gh-pages-branch
 
 #创建一个新的仓库
-# git config --global user.email "$GH_EMAIL" >/
-# dev/null 2>&1
-# git config --global user.name "$GH_NAME" >/
-# dev/null 2>&1
+git config --global user.email "$GH_EMAIL" 
+git config --global user.name "$GH_NAME"
 git init
 git remote add --fetch origin "$remote"
 
