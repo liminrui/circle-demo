aa98c2f5da43c88946918f45928a5cb78254a46d
Admin <209695793@qq.com>
2024-02-22 15:56:18 +0800

    添加自动化部署

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/circle-demo',
+  base: process.env.NODE_ENV === 'production' ? '/circle-demo' : '',
   plugins: [vue()],
 })
